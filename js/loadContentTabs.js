7a8729a71b16307a43e8f886a844521cd151874b
HaselLoyance <xmasyolka@gmail.com>
2016-06-25 18:14:40 -0500

    Loading content
    Noew there is a new algorithm to load content from other html pages. R E S P O N S I V E N E S S

--- js/loadContentTabs.js
+++ js/loadContentTabs.js
@@ -12,44 +12,39 @@ function loadContent(nameOfPage, tabNumber, calledFromOtherHTML = false) {
 
  window.parent.$("body").promise().done(function(){
   if(tabNumber != -1) {
-  var tabLinks = parent.document.getElementsByClassName("tabLink");
+   var tabLinks = parent.document.getElementsByClassName("tabLink");
 
-  if(calledFromOtherHTML) parent.document.opened = 'none'; //will always redraw block of text in content
+   if(calledFromOtherHTML) parent.document.opened = 'none'; //will always redraw block of text in content
 
-  //if new block of text is requested
-  if(parent.document.opened != window.location.href.split("#")[0] + nameOfPage +'.html') {
+   //if new block of text is requested
+   if(parent.document.opened != window.location.href.split("#")[0] + nameOfPage +'.html') {
 
-   //while it loads - put loading symbol
-   parent.document.getElementById("contentWrap").innerHTML='<p style="font-size:100px" class="w3-center"><i class="fa fa-refresh"></i></p>';
+    //while it loads - put loading symbol
+    parent.document.getElementById("contentWrap").innerHTML='<p style="font-size:100px" class="w3-center"><i class="fa fa-refresh"></i></p>';
 
-   for (i = 0; i < tabLinks.length; i++) { //make all tabs unselected
+    for (i = 0; i < tabLinks.length; i++) { //make all tabs unselected
      tabLinks[i].classList.remove("w3-border-green");
      tabLinks[i].classList.remove("w3-light-green");
-   }
+    }
 
-   //make selectd tab selected
-   tabLinks[tabNumber].classList.add("w3-border-green");
-   tabLinks[tabNumber].classList.add("w3-light-green");
+    //make selectd tab selected
+    tabLinks[tabNumber].classList.add("w3-border-green");
+    tabLinks[tabNumber].classList.add("w3-light-green");
 
-   //same for the mobile menu
-   tabLinks[(tabNumber+4)].classList.add("w3-border-green");
-   tabLinks[(tabNumber+4)].classList.add("w3-light-green");
+    //same for the mobile menu
+    tabLinks[(tabNumber+4)].classList.add("w3-border-green");
+    tabLinks[(tabNumber+4)].classList.add("w3-light-green");
 
-   //sets current opened window
-   parent.document.opened = parent.window.location.href.split("#")[0] + nameOfPage +'.html';
+    //sets current opened window
+    parent.document.opened = parent.window.location.href.split("#")[0] + nameOfPage +'.html';
 
-   //loads new content
-   var contentXRH= new XMLHttpRequest();
-   contentXRH.open('GET', '/mainInfo/' + nameOfPage+'.html', true);
-   contentXRH.onreadystatechange = function() {
-    if (contentXRH.readyState == 4 && contentXRH.status == 200) {
-     parent.document.getElementById("contentWrap").innerHTML= this.responseText;
+    //loads new content
+    $.get('/mainInfo/' + nameOfPage+'.html', function(data) {
+     document.getElementById('contentWrap').innerHTML= data;
      $('.javascript').each(function() {
       eval($(this).text());
-     });
-    }
-   };
-   contentXRH.send();
+      });
+    });
    }
   }
  });
